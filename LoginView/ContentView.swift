d5fe136ba6ca828c1997aeac09c42cb7262f5421
ArunPrasanth R <arun.ap.0810@gmail.com>
2025-12-07 10:33:51 +0800

    Initial Commit

---
+++ LoginView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoginView
+//
+//  Created by ArunPrasanth R on 7/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoginView/LoginViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewApp.swift
+//  LoginView
+//
+//  Created by ArunPrasanth R on 7/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LoginViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
